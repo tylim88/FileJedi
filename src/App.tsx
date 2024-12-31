8344e53a95236391679dcd3aee968da989945497
tylim <limteikyee@gmail.com>
2025-01-01 05:48:20 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Loader, Container, Center, Anchor, Text } from '@mantine/core'
+import { Loader, Container, Anchor, Text } from '@mantine/core'
 import { Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from './component'
 import { Suspense, useEffect } from 'react'
@@ -10,19 +10,18 @@ export const App = () => {
 	}, [])
 	return (
 		<Container h="100%">
-			<Center h="100%" w="100%" style={{ flexDirection: 'column' }}>
-				<Text>
-					This website is temporary down, please come back later or get the code
-					here:
-				</Text>
-				<Anchor
-					c="white"
-					href="https://github.com/tylim88/FileJedi"
-					underline="always"
-				>
-					Github
-				</Anchor>
-			</Center>
+			<Text>
+				This website is temporary down, please come back later or get the code
+				here:
+			</Text>
+			<Anchor
+				c="white"
+				href="https://github.com/tylim88/FileJedi"
+				underline="always"
+			>
+				Github
+			</Anchor>
+			<Outlet />
 			<Suspense fallback={<Loader />}>
 				<TanStackRouterDevtools />
 			</Suspense>
