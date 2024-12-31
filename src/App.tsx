3c91eafb4737b2d1db1b100a08a9de158c7b8050
tylim <limteikyee@gmail.com>
2024-12-31 23:15:51 +0800

    add github link on down page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Loader, Container, Center } from '@mantine/core'
+import { Loader, Container, Center, Anchor, Text } from '@mantine/core'
 import { Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from './component'
 import { Suspense, useEffect } from 'react'
@@ -10,8 +10,18 @@ export const App = () => {
 	}, [])
 	return (
 		<Container h="100%">
-			<Center h="100%" w="100%">
-				This website is temporary down, please check back later.
+			<Center h="100%" w="100%" style={{ flexDirection: 'column' }}>
+				<Text>
+					This website is temporary down, please come back later or get the code
+					here:
+				</Text>
+				<Anchor
+					c="white"
+					href="https://github.com/tylim88/FileJedi"
+					underline="always"
+				>
+					Github
+				</Anchor>
 			</Center>
 			<Suspense fallback={<Loader />}>
 				<TanStackRouterDevtools />
