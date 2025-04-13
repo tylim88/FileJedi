e1d096efb3664afdb4a71acd6cd4f4ad95ef1b18
tylim <limteikyee@gmail.com>
2025-04-13 22:21:27 +0800

    add header

---
+++ src/app/motivation/page.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@mantine/core'
+
+export default function Audio() {
+	return <Text>123</Text>
+}
---
+++ src/components/Link.tsx
@@ -0,0 +1,24 @@
+import Link_ from 'next/link'
+import type React from 'react'
+import type { StrictOmit } from 'ts-essentials'
+
+type propsLink = React.ComponentProps<typeof Link_>
+
+export const Link = ({
+	href,
+	...props
+}: StrictOmit<propsLink, 'href'> &
+	(
+		| {
+				href: '/' | '/audio' | '/motivation'
+		  }
+		| { href: `https://${string}`; target?: never }
+	)) => {
+	return (
+		<Link_
+			target={href.toString().includes('https://') ? '_blank' : ''}
+			{...props}
+			href={href}
+		/>
+	)
+}
