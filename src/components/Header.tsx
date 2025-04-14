edd52f79c709529c328d7439cdc034db3ffa9ce6
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2025-04-14 15:27:52 +0800

    add drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,109 @@
 'use client'
-import { Flex, Text, ActionIcon } from '@mantine/core'
+import {
+	Flex,
+	Text,
+	ActionIcon,
+	Burger,
+	Drawer,
+	NavLink,
+	DEFAULT_THEME,
+} from '@mantine/core'
 import { Link } from './Link'
-import { BsGithub } from 'react-icons/bs'
+import { BsGithub, BsExclamationLg } from 'react-icons/bs'
+import { useDisclosure } from '@mantine/hooks'
+import { usePathname } from 'next/navigation'
+import { LiaFileAudio, LiaFileVideo } from 'react-icons/lia'
 
 const arr = [
-	{ href: '/video', label: 'Video' },
-	{ href: '/audio', label: 'Audio' },
-	{ href: '/motivation', label: 'Motivation' },
+	{
+		href: '/video',
+		label: 'Video',
+		Icon: LiaFileVideo,
+		title: 'go to video convertor page',
+	},
+	{
+		href: '/audio',
+		label: 'Audio',
+		Icon: LiaFileAudio,
+		title: 'go to audio convertor page',
+	},
+	{
+		href: '/motivation',
+		label: 'Motivation',
+		Icon: BsExclamationLg,
+		title: 'go to about page',
+	},
 ] as const
 
 export const Header = () => {
+	const [opened, { toggle, close }] = useDisclosure()
+	const pathname = usePathname()
+
 	return (
-		<Flex
-			component="nav"
-			w="100%"
-			justify="space-between"
-			pt="md"
-			align="center"
-			display={{ base: 'none', sm: 'flex' }}
-		>
-			<Text component={Link} href="/" fw={800} title="logo" size="xl">
+		<Flex w="100%" justify="space-between" pt="md" align="center">
+			<Text component={Link} href="/" fw={800} title="go to homepage" size="xl">
 				FILE JEDI
 			</Text>
-			<Flex gap="xl" align="center">
-				{arr.map(({ href, label }) => (
-					<Text component={Link} href={href} size="xl" fw={600} key={href}>
-						{label}
+			<Burger
+				opened={opened}
+				onClick={toggle}
+				aria-label="Toggle navigation"
+				display={{ sm: 'none' }}
+				title="open navigation drawer"
+			/>
+			<Drawer
+				title={
+					<Text
+						component={Link}
+						href="/"
+						fw={800}
+						title="go to homepage"
+						size="xl"
+					>
+						FILE JEDI
 					</Text>
+				}
+				closeButtonProps={{ 'aria-label': 'Close drawer' }}
+				opened={opened}
+				onClose={close}
+				position="right"
+				component="nav"
+				size="100%"
+			>
+				{arr.map(({ href, label, Icon, title }) => (
+					<NavLink
+						color="black"
+						component={Link}
+						active={pathname === href}
+						href={href}
+						key={href}
+						label={label}
+						leftSection={Icon && <Icon />}
+						onClick={close}
+						title={title}
+						aria-label={title}
+					/>
+				))}
+			</Drawer>
+			<Flex
+				component="nav"
+				gap="xl"
+				align="center"
+				display={{ base: 'none', sm: 'flex' }}
+			>
+				{arr.map(({ href, label, title }) => (
+					<NavLink
+						color="black"
+						component={Link}
+						active={pathname === href}
+						href={href}
+						styles={{
+							label: { fontSize: DEFAULT_THEME.fontSizes.xl, fontWeight: 600 },
+						}}
+						key={href}
+						label={label}
+						title={title}
+					/>
 				))}
 				<ActionIcon
 					component={Link}
