308b1e1f6962bd50918132db0786db721a3b3173
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2025-04-15 14:11:46 +0800

    simplify seperating dev and prod core.wasm

--- src/config.dev.ts
+++ src/config.dev.ts
@@ -1,3 +1,4 @@
+import wasmURL from './core.wasm?url'
 export const isProduction: boolean = false
 export const feedbackURL = 'https://localhost:8787/'
-export const wasmURL = '/core.wasm'
+export { wasmURL }
