128195ff5fd898541db02159d0e9fc25bf859c6e
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2025-04-15 14:43:22 +0800

    move worker to storage

--- src/config.dev.ts
+++ src/config.dev.ts
@@ -2,3 +2,4 @@ import wasmURL from './core.wasm?url'
 export const isProduction: boolean = false
 export const feedbackURL = 'https://localhost:8787/'
 export { wasmURL }
+export const workerURL = '/worker.js'
--- src/config.prod.ts
+++ src/config.prod.ts
@@ -1,3 +1,4 @@
 export const isProduction: boolean = true
 export const feedbackURL = 'https://filejedi.com/feedback'
 export const wasmURL = 'https://api.filejedi.com/ffmpeg-core.wasm'
+export const workerURL = 'https://api.filejedi.com/worker.js'
