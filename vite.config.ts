9a3a53c496b53ab4f94ef8432d131e750fa869ee
Lim Teik Yee <teikyee.lim@it.destijltech.com>
2025-04-15 15:01:56 +0800

    change header

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 		headers: {
 			'Cross-Origin-Opener-Policy': 'same-origin',
 			'Cross-Origin-Embedder-Policy': 'require-corp',
+			'Cross-Origin-Resource-Policy': 'same-origin',
 		},
 	},
 })
